22657ef143695daf83ec78f1c39d4bf041537b01
Christopher Liebman <liebman@zod.com>
2020-09-27 10:22:58 -0700

    improve Sat tracked counting graph

--- src/PageSats.cpp
+++ src/PageSats.cpp
@@ -18,7 +18,7 @@ PageSats::PageSats(GPS& gps) : _gps(gps)
         _container_style.setBorderWidth(LV_STATE_DEFAULT, 0);
         _container_style.setShadowWidth(LV_STATE_DEFAULT, 0);
 
-        _chart_style.setRadius(LV_STATE_DEFAULT, 0);
+        //_chart_style.setRadius(LV_STATE_DEFAULT, 0);
         _chart_style.setSize(LV_STATE_DEFAULT, 1);
         _chart_style.setLineWidth(LV_STATE_DEFAULT, 1);
 
@@ -28,21 +28,28 @@ PageSats::PageSats(GPS& gps) : _gps(gps)
         LVContainer* cont = new LVContainer(_page);
         cont->setFit(LV_FIT_PARENT/*, LV_FIT_TIGHT*/);
         cont->addStyle(LV_CONT_PART_MAIN, &_container_style);
-        cont->setLayout(LV_LAYOUT_COLUMN_LEFT);
+        cont->setLayout(LV_LAYOUT_COLUMN_MID);
         cont->align(nullptr, LV_ALIGN_CENTER, 0, 0);
         cont->setDragParent(true);
 
         _chart = new LVChart(cont);
         _chart->addStyle(LV_CHART_PART_SERIES, &_chart_style);
-        _chart->setSize(300, 200);
+        _chart->setSize(315, 200);
         _chart->setType(LV_CHART_TYPE_LINE);
         _chart->setDragParent(true);
-        _chart->setPointCount(120);
+        _chart->setPointCount(60);
+        _chart->setDivLineCount(4, 0);
         _chart->setYRange(LV_CHART_AXIS_PRIMARY_Y, 0, 25);
-        _tracked = _chart->addSeries(LV_COLOR_RED);
+        _chart->setYTickLength(10, 5);
+        _chart->setYTickTexts("0\n5\n10\n15\n20\n25", 5, LV_CHART_AXIS_DRAW_LAST_TICK|LV_CHART_AXIS_INVERSE_LABELS_ORDER);
+        _chart->setStylePadLeft(LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, 35);
+        _chart->setStyleBorderWidth(LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, 0);
+
+        _tracked_max_series = _chart->addSeries(LV_COLOR_LIME);
+        _tracked_min_series = _chart->addSeries(LV_COLOR_ORANGE);
 
         ESP_LOGI(TAG, "creating task");
-        lv_task_create(task, 1000, LV_TASK_PRIO_LOW, this);
+        lv_task_create(task, 500, LV_TASK_PRIO_LOW, this);
     });
 }
 
@@ -58,5 +65,24 @@ void PageSats::task(lv_task_t *task)
 
 void PageSats::update()
 {
-    _chart->setNext(_tracked, _gps.getSatsTracked());
+    int tracked = _gps.getSatsTracked();
+    if (tracked > _tracked_max)
+    {
+        _tracked_max = tracked;
+    }
+    if (tracked < _tracked_min)
+    {
+        _tracked_min = tracked;
+    }
+
+    time_t now = time(nullptr);
+    // update the chart once a minute
+    if (now > (_last_time + 60))
+    {
+        _chart->setNext(_tracked_min_series, _tracked_min);
+        _chart->setNext(_tracked_max_series, _tracked_max);
+        _tracked_min = tracked;
+        _tracked_max = tracked;
+        _last_time = now;
+    }
 }
--- src/PageSats.h
+++ src/PageSats.h
@@ -16,9 +16,15 @@ private:
     GPS&     _gps;
     LVPage*  _page;
     LVChart* _chart;
-    lv_chart_series_t* _tracked;
+    lv_chart_series_t* _tracked_max_series;
+    lv_chart_series_t* _tracked_min_series;
     LVStyle  _container_style;
     LVStyle  _chart_style;
+
+    uint16_t _tracked_max = 0;
+    uint16_t _tracked_min = 0;
+
+    time_t _last_time = 0;
 };
 
 #endif // _PAGE_SATS_H_
