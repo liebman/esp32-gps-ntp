26f951bbeca7c3c7d535fdd74fd57cfcaa4af0c2
Christopher Liebman <liebman@zod.com>
2020-10-03 06:03:59 -0700

    configure fort esp32ntp-v0.1 board!

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 #include "driver/i2c.h"
 #include "esp_log.h"
 
-#include "TeaMakerPins.h"
 #include "Display.h"
-#include "MCP23017.h"
 #include "GPS.h"
 #include "PageGPS.h"
 #include "PageSats.h"
@@ -19,57 +17,40 @@ extern "C" {
 }
 #endif
 
-#define GPS_RX_PIN (GPIO_NUM_4)
-#define GPS_TX_PIN (GPIO_NUM_2)
+#define GPS_RX_PIN (GPIO_NUM_33)
+#define GPS_TX_PIN (GPIO_NUM_32)
 #define GPS_PPS_PIN (GPIO_NUM_26)
+#define SDA_PIN (GPIO_NUM_16)
+#define SCL_PIN (GPIO_NUM_17)
+#define TCH_IRQ_PIN (GPIO_NUM_36)
+#define TCH_IRQ_SEL (GPIO_SEL_36)
+#define TFT_LED_PIN (GPIO_NUM_4)
+#define TFT_LED_SEL (GPIO_SEL_4)
 
 static const char* TAG = "app_main";
-static MCP23017 mcp23017;
 static GPS gps(GPS_PPS_PIN);
 
 void app_main() 
 {
     ESP_LOGI(TAG, "Starting with priority %d", uxTaskPriorityGet(nullptr));
 
-    // configure reset gpio as output, no interrupts or pull-up/down
-    gpio_config_t io_conf;
-    io_conf.pin_bit_mask = MCP23017_RST_SEL;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
-    esp_err_t err = gpio_config(&io_conf);
-    if (err != ESP_OK)
-    {
-        ESP_LOGE(TAG, "failed to init MCP23017 reset pin as output: %d '%s'", err, esp_err_to_name(err));
-    }
-    // turn the backlight on.
-    ESP_LOGI(TAG,  "removing reset from MCP23017");
-    gpio_set_level(MCP23017_RST_PIN, 1);
-
     // initialize I2C_NUM_0 for the MCP23017T
-    err = i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+    esp_err_t err = i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
     if (err != ESP_OK)
     {
         ESP_LOGE(TAG, "i2c_driver_install: I2C_NUM_%d %d (%s)", I2C_NUM_0, err, esp_err_to_name(err));
     }
     i2c_config_t conf;
     conf.mode = I2C_MODE_MASTER;
-    conf.sda_io_num = TM_PIN_SDA;
-    conf.scl_io_num = TM_PIN_SCL;
-    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.sda_io_num = SDA_PIN;
+    conf.scl_io_num = SCL_PIN;
+    conf.sda_pullup_en = GPIO_PULLUP_DISABLE;
+    conf.scl_pullup_en = GPIO_PULLUP_DISABLE;
     conf.master.clk_speed = 400000;
     err = i2c_param_config(I2C_NUM_0, &conf);
     if (err != ESP_OK)
     {
         ESP_LOGE(TAG, "i2c_param_config: I2C_NUM_%d %d (%s)", I2C_NUM_0, err, esp_err_to_name(err));
-    }
-
-    // initialize the mcp23017
-    if (!mcp23017.begin())
-    {
-        ESP_LOGE(TAG, "failed to initialize the mcp23017!");
     }
 
     // initialize the display
@@ -85,16 +66,29 @@ void app_main()
 
     // turn the backlight on.
     ESP_LOGI(TAG,  "turning backlight on");
-    mcp23017.portMode(MCP23017::B, 0, TM_BACKLIGHT_BIT);
-    mcp23017.portWrite(MCP23017::B, TM_BACKLIGHT_BIT, TM_BACKLIGHT_BIT);
 
-    if (!gps.begin(GPS_TX_PIN, GPS_RX_PIN))
+    gpio_config_t io_conf;
+    io_conf.pin_bit_mask = TFT_LED_SEL;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    err = gpio_config(&io_conf);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "failed to init backlight pin as output: %d '%s'", err, esp_err_to_name(err));
+    }
+    // turn the backlight on.
+    gpio_set_level(TFT_LED_PIN, 1);
+
+    // swap rx/tx as GPS_RX is our TX!
+    if (!gps.begin(GPS_RX_PIN, GPS_TX_PIN))
     {
         ESP_LOGE(TAG, "failed to start gps!");
     }
 
     // lets monitor the touch input.
-    io_conf.pin_bit_mask = TOUCH_IRQ_SEL;
+    io_conf.pin_bit_mask = TCH_IRQ_SEL;
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.intr_type = GPIO_INTR_DISABLE;
     io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
@@ -106,7 +100,7 @@ void app_main()
     while(true)
     {
         time_t now = time(nullptr);
-        if (gpio_get_level(TOUCH_IRQ_PIN) == 0)
+        if (gpio_get_level(TCH_IRQ_PIN) == 0)
         {
             last_touch = now;
         }
@@ -116,14 +110,14 @@ void app_main()
             if (is_on)
             {
                 ESP_LOGI(TAG, "turning OFF backlight");
-                mcp23017.portWrite(MCP23017::B, 0, TM_BACKLIGHT_BIT);
+                gpio_set_level(TFT_LED_PIN, 0);
                 is_on = false;
             }
         }
         else if (!is_on)
         {
             ESP_LOGI(TAG, "turning ON backlight");
-            mcp23017.portWrite(MCP23017::B, TM_BACKLIGHT_BIT, TM_BACKLIGHT_BIT);
+            gpio_set_level(TFT_LED_PIN, 1);
             is_on = true;
         }
 
