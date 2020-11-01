d503728d2f65d05cca88c6c7cba0b036eb778644
Christopher Liebman <liebman@zod.com>
2020-11-01 11:25:25 -0800

    improved network

--- src/Network.cpp
+++ src/Network.cpp
@@ -7,7 +7,7 @@
 #include "memory.h"
 
 static const char* TAG = "Network";
-static EventGroupHandle_t wifi_events;
+static EventGroupHandle_t network_status;
 
 Network::Network()
 {
@@ -25,7 +25,8 @@ Network& Network::getNetwork()
 
 bool Network::begin()
 {
-    wifi_events = xEventGroupCreate();
+    ESP_LOGI(TAG, "::begin starting");
+    network_status = xEventGroupCreate();
 
     ESP_ERROR_CHECK(esp_netif_init());
 
@@ -35,8 +36,8 @@ bool Network::begin()
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &Network::eventHandler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &Network::eventHandler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &Network::eventHandler, this));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &Network::eventHandler, this));
 
     wifi_config_t wifi_config;
     memset(&wifi_config, 0, sizeof(wifi_config));
@@ -48,50 +49,67 @@ bool Network::begin()
     {
         wifi_config.sta.password[i] = WIFI_PASS[i];
     }
+    wifi_config.sta.scan_method = WIFI_ALL_CHANNEL_SCAN;
     wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
     wifi_config.sta.pmf_cfg.capable  = true;
     wifi_config.sta.pmf_cfg.required = false;
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE)); // disable power save as it adds up to 300ms or more in latemcy!
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
     ESP_ERROR_CHECK(esp_wifi_start() );
 
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
-
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
-    EventBits_t bits = xEventGroupWaitBits(wifi_events,
-            CONNECTED,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
-
-    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-     * happened. */
-    if (bits & CONNECTED) {
-        ESP_LOGI(TAG, "connected to ap SSID:%s", WIFI_SSID);
-    } else {
-        ESP_LOGE(TAG, "UNEXPECTED EVENT");
-    }
+    ESP_LOGI(TAG, "::begin finished");
 
     return true;
 }
 
-void Network::eventHandler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+bool Network::hasIP()
 {
+    EventBits_t bits = xEventGroupWaitBits(network_status, HAS_IP, pdFALSE, pdFALSE, 0);
+    return (bits & HAS_IP) == HAS_IP;
+}
+
+esp_ip4_addr_t Network::getIPAddress(char* buf, size_t size)
+{
+    if (buf != nullptr && size != 0)
+    {
+        strncpy(buf, _ip_str, size);
+    }
+    return _ip;
+}
+
+uint32_t Network::waitFor(Status status, TickType_t wait)
+{
+    return xEventGroupWaitBits(network_status, status, pdFALSE, pdFALSE, wait);
+}
+
+void Network::eventHandler(void* data, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+    Network* net = (Network*)data;
+
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
     {
+        xEventGroupClearBits(network_status, HAS_IP);
+        memset(&net->_ip, 0, sizeof(net->_ip));
+        net->_ip_str[0] = '\0';
         esp_wifi_connect();
+        ESP_LOGI(TAG, "station was started, initiated connect to AP");
     }
     else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
     {
+        xEventGroupClearBits(network_status, HAS_IP);
+        memset(&net->_ip, 0, sizeof(net->_ip));
+        net->_ip_str[0] = '\0';
         esp_wifi_connect();
-        ESP_LOGI(TAG, "retry to connect to the AP");
+        ESP_LOGI(TAG, "disconnected! retry to connect to the AP");
     }
     else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
     {
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        xEventGroupSetBits(wifi_events, CONNECTED);
+        net->_ip = event->ip_info.ip;
+        snprintf(net->_ip_str, sizeof(net->_ip_str), IPSTR, IP2STR(&event->ip_info.ip));
+        xEventGroupSetBits(network_status, HAS_IP);
+        ESP_LOGI(TAG, "got ip: %s", net->_ip_str);
     }
 }
--- src/Network.h
+++ src/Network.h
@@ -6,16 +6,20 @@
 class Network
 {
 public:
-    enum NetworkStatus
-    {
-        CONNECTED = BIT0,
-    };
+    using Status = uint32_t;
+    static const Status HAS_IP       = BIT0;
+
     static Network& getNetwork();
     ~Network();
     bool begin();
+    bool hasIP();
+    esp_ip4_addr_t getIPAddress(char* buf = nullptr, size_t size = 0);
+    uint32_t waitFor(Status status, TickType_t wait = portMAX_DELAY);
 
 private:
     Network();
+    esp_ip4_addr_t _ip;
+    char           _ip_str[16];
     static void eventHandler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
 };
 
